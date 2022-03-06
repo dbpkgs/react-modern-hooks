3cb1e15f3653e9df6b5658cc037bdd501d583e26
sheldon-welinga <sheldonwelinga@gmail.com>
2022-03-07 00:59:10 +0300

    Update the usePageTitle hook

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
 
+/**
+ * usePageTitle - Hook to update the page title of a document url
+ *
+ * @param {string} title - The title to be set as new document title on the page
+ */
 const usePageTitle = (title: string): void => {
   useEffect(() => {
     if (document && typeof document !== 'undefined') {
