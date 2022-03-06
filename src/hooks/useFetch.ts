4426bd683dc581252e65837b1f01c9ceeb073e40
sheldon-welinga <sheldonwelinga@gmail.com>
2022-03-07 00:34:09 +0300

    Update the useFetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,6 +2,12 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Options } from '../types';
 
+/**
+ * useFetch - Hook for prefetching data from a API endpoint
+ *
+ * @param {string} url - The API endpoint to fetch data from
+ * @param {Options} options - optional parameters allowed to be passed when fetching data from an endpoint
+ */
 const useFetch = <T = any>(url: string, options?: Options) => {
   const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState<unknown>(null);
