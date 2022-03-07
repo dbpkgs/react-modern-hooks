de62469895caa0a8c342924cf120f014d104de5c
sheldon-welinga <sheldonwelinga@gmail.com>
2022-03-08 00:04:53 +0300

    Update the exported hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -6,3 +6,6 @@ export { default as useOs } from './useOs';
 export { default as useSearch } from './useSearch';
 export { default as usePageTitle } from './usePageTitle';
 export { default as useImageDownload } from './useImageDownload';
+export { default as useFocus } from './useFocus';
+export { default as useDebounce } from './useDebounce';
+export { default as useOnline } from './useOnline';
