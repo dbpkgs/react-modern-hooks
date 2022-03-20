8b0b7e8d097a3b4be2ad58d14919942b3462cc08
sheldon-welinga <sheldonwelinga@gmail.com>
2022-03-20 17:33:06 +0300

    fix: default and named file imports and exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,60 @@
-// export { default as hooks } from './hooks';
-import * as hooks from 'hooks';
+import useFetch from 'hooks/useFetch';
+import useResize from 'hooks/useResize';
+import useStorage from 'hooks/useStorage';
+import useStateCallback from 'hooks/useStateCallback';
+import useOs from 'hooks/useOs';
+import useSearch from 'hooks/useSearch';
+import usePageTitle from 'hooks/usePageTitle';
+import useImageDownload from 'hooks/useImageDownload';
+import useFocus from 'hooks/useFocus';
+import useDebounce from 'hooks/useDebounce';
+import useOnline from 'hooks/useOnline';
+import useDeviceDetect from 'hooks/useDeviceDetect';
+import useNetwork from 'hooks/useNetwork';
+import useFullScreen from 'hooks/useFullScreen';
+import useGeolocation from 'hooks/useGeolocation';
+import useCopyToClipboad from 'hooks/useCopyToClipboard';
+import useOnClickOutside from 'hooks/useOnClickOutside';
+import useSelectedText from 'hooks/useSelectedText';
 
-export default hooks;
+export {
+  useFetch,
+  useResize,
+  useStorage,
+  useStateCallback,
+  useOs,
+  useSearch,
+  usePageTitle,
+  useImageDownload,
+  useFocus,
+  useDebounce,
+  useOnline,
+  useDeviceDetect,
+  useNetwork,
+  useFullScreen,
+  useGeolocation,
+  useOnClickOutside,
+  useSelectedText,
+  useCopyToClipboad,
+};
+
+export default {
+  useFetch,
+  useResize,
+  useStorage,
+  useStateCallback,
+  useOs,
+  useSearch,
+  usePageTitle,
+  useImageDownload,
+  useFocus,
+  useDebounce,
+  useOnline,
+  useDeviceDetect,
+  useNetwork,
+  useFullScreen,
+  useGeolocation,
+  useOnClickOutside,
+  useSelectedText,
+  useCopyToClipboad,
+};
